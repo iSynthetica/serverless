afedbb88b01753bda0aa8ebd07d65656e2576653
Synthetica <i.synthetica@gmail.com>
2020-03-12 16:08:15 +0200

    Show payloads

--- integration/aws/index.js
+++ integration/aws/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 module.exports.process = async event => {
+  let step = event.pathParameters;
+  let payload = JSON.parse(event.body);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        step: step,
+        payload: payload,
+        input: event
       },
       null,
       2
