01c6cf5b6da6fa3d7ee6471cff78e60b85443c55
Synthetica <i.synthetica@gmail.com>
2020-03-12 17:38:37 +0200

    Validate with schem 1

--- controllers/register/requestSchema.js
+++ controllers/register/requestSchema.js
@@ -1,9 +1,9 @@
 const Joi = require("@hapi/joi");
 
 // Strict Request Schema Validation
-module.exports = {
+module.exports = Joi.object().keys({
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
     email: Joi.string().required(),
     password: Joi.string().required(),
-};
+});
