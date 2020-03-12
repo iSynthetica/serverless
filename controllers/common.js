9627eb17ae16d719f851a16228e31dad9dd8701c
Synthetica <i.synthetica@gmail.com>
2020-03-12 17:53:19 +0200

    Validate with schem 2

--- controllers/common.js
+++ controllers/common.js
@@ -1,4 +1,3 @@
-const Joi = require('@hapi/joi');
 const empty = require('is-empty')
 
 // Will validate an object against a JOI schema
@@ -6,16 +5,11 @@ exports.validateParams = (object, schema, allowUnknown = true) => {
     if (empty(schema)) {
         return object;
     }
-    const result = schema.validate(object);
+
+    const { value, error, warning } = schema.validate(object);
     // const result = Joi.validate(object, schema, { allowUnknown: allowUnknown, abortEarly: false });
-    if (result.error !== null) {
-        let error_messages = [];
-
-        result.error.details.forEach((error) => {
-            error_messages.push({ message: error.message });
-        });
-
-        return Promise.reject({ statusCode: 400, body: { messages: error_messages }, isJoi: true })
+    if (error) {
+        return Promise.reject({ statusCode: 400, body: { messages: {"error":"error"} }, isJoi: true })
     }
-    return result.value; // We return the validated value with any type conversions and other modifiers applied
+    return value; // We return the validated value with any type conversions and other modifiers applied
 }
