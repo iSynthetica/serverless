a80bc9b9ef81bc2a366942f7e9b8b2143aede287
Synthetica <i.synthetica@gmail.com>
2020-03-13 10:41:05 +0200

    Validate with schem 5

--- controllers/common.js
+++ controllers/common.js
@@ -6,10 +6,12 @@ exports.validateParams = (object, schema, allowUnknown = true) => {
         return object;
     }
 
-    const { value, error, warning } = schema.validate(object);
-    // const result = Joi.validate(object, schema, { allowUnknown: allowUnknown, abortEarly: false });
-    if (error) {
-        return Promise.reject({ statusCode: 400, body: { messages: {"error":"error"} }, isJoi: true })
-    }
-    return value; // We return the validated value with any type conversions and other modifiers applied
+    console.log(object);
+
+    // const { value, error, warning } = schema.validate(object);
+    // // const result = Joi.validate(object, schema, { allowUnknown: allowUnknown, abortEarly: false });
+    // if (error) {
+    //     return Promise.reject({ statusCode: 400, body: { messages: {"error":"error"} }, isJoi: true })
+    // }
+    // return value; // We return the validated value with any type conversions and other modifiers applied
 }
